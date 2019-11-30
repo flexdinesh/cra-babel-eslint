d212700a88f2e4bbc9883a81aa3b6578b15d1598
Dinesh Pandiyan <flexdinesh@gmail.com>
2019-11-30 13:56:54 +1100

    Update log-render logic

--- plugins/babel-transform-log-render.test.js
+++ plugins/babel-transform-log-render.test.js
@@ -1,19 +1,64 @@
-import { transformSync } from "@babel/core";
-import logRenderPlugin from "./babel-transform-log-render";
+import { transformSync } from '@babel/core';
+import logRenderPlugin from './babel-transform-log-render';
 
-const HelloWorld = `
-const HelloWorld = () => {
-  const [val, setVal] = useState(true);
-  const [anotherVal, setAnotherVal] = useState(100);
-  return <span>Hello World</span>;
+const defaultTestConfig = {
+  ast: true,
+  plugins: ['@babel/plugin-transform-react-jsx', logRenderPlugin]
 };
-`;
 
-test("inject console.log", () => {
-  const { code } = transformSync(HelloWorld, {
-    ast: true,
-    plugins: ["@babel/plugin-transform-react-jsx", logRenderPlugin]
+describe('Add log statement to React Components', () => {
+  test('ArrowFunctionExpression with BlockStatement', () => {
+    const codeLiteral = `
+      const HelloWorld = () => {
+        return <span>Hello World</span>;
+      };
+    `;
+    const { code } = transformSync(codeLiteral, defaultTestConfig);
+    expect(code).toMatchSnapshot();
   });
 
-  expect(code).toMatchSnapshot();
+  // THIS DOESN'T WORK YET
+  test('ArrowFunctionExpression with implicit return', () => {
+    const codeLiteral = `
+      const HelloWorld = (props) => <span>Hello World</span>;
+    `;
+    const { code } = transformSync(codeLiteral, defaultTestConfig);
+
+    expect(code).toMatchSnapshot();
+  });
+
+  test('FunctionDelaration with BlockStatement', () => {
+    const codeLiteral = `
+      function HelloWorld() {
+        return <span>Hello World</span>;
+      }
+    `;
+    const { code } = transformSync(codeLiteral, defaultTestConfig);
+
+    expect(code).toMatchSnapshot();
+  });
+
+  test('FunctionExpression with BlockStatement', () => {
+    const codeLiteral = `
+      const HelloWorld = function() {
+        return <span>Hello World</span>;
+      }
+    `;
+    const { code } = transformSync(codeLiteral, defaultTestConfig);
+
+    expect(code).toMatchSnapshot();
+  });
+
+  test('Class Component with render method', () => {
+    const codeLiteral = `
+      class HelloWorld extends React.Component {
+        render() {
+          return <span>Hello World</span>
+        }
+      }
+    `;
+    const { code } = transformSync(codeLiteral, defaultTestConfig);
+
+    expect(code).toMatchSnapshot();
+  });
 });
